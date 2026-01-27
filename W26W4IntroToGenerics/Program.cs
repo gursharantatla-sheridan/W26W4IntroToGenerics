6a63521a22d8eda20a75923269955a5a2f6a73c4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-27 17:11:42 -0500

    Add project files.

---
+++ W26W4IntroToGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace W26W4IntroToGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
