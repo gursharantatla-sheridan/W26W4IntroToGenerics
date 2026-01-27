7f528495f84dfbaad95aef3d8e73998ca02d8d3f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-27 17:41:16 -0500

    boxing and unboxing

--- W26W4IntroToGenerics/Program.cs
+++ W26W4IntroToGenerics/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // boxing - assigning value-type to ref-type
+            int i = 5;
+            object o = i;
+            //o = "hello";
+
+            // unboxing - assigning ref-type to value
+            if (o is int)
+                i = (int)o;
+
+
+            //if (AreEqual(5.4, "5.4"))
+            if (AreEqual<int>(3, 3))
+                Console.WriteLine("Both are equal");
+            else
+                Console.WriteLine("Both are not equal");
+        }
+
+        // normal, non-generic method
+        // not type-safe
+        // boxing - performance issue
+        static bool AreEqual(object value1, object value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        // generic method
+        // type-safe
+        // no boxing - better performance
+        static bool AreEqual<T>(T value1, T value2)
+        {
+            return value1.Equals(value2);
         }
     }
 }
