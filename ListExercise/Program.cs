3a559e616b7e9575a6ff1f205ea03dd8f86bf898
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-27 18:56:24 -0500

    index of

--- ListExercise/Program.cs
+++ ListExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee emp1 = new Employee(101, "John", 5000);
+            Employee emp2 = new Employee(102, "Anne", 7000);
+            Employee emp3 = new Employee(103, "Mark", 3000);
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+
+            employees.Insert(1, emp3);
+            employees.Remove(emp3);
+
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine(emp + "\n");
+            }
+            Console.WriteLine("\n\n");
+
+
+            // Searching
+
+            // IndexOf
+            int index = employees.IndexOf(emp2);
+
+            if (index >= 0)
+                Console.WriteLine("Item exists");
+            else
+                Console.WriteLine("Item doesn't exist");
         }
     }
 }
