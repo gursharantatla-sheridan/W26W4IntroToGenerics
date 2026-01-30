681328c2d5d5385fda72f840d83333dbd199b45d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-30 09:54:06 -0500

    queue eg

---
+++ QueueExample/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace QueueExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            //int num = queue[2];
+
+            foreach(int i in queue)
+                Console.WriteLine(i);
+
+
+            int num = queue.Dequeue();
+            Console.WriteLine("\nnum = " + num);
+            Console.WriteLine("Total items: " + queue.Count);
+            
+            foreach (int i in queue)
+                Console.WriteLine(i);
+
+
+            num = queue.Peek();
+            Console.WriteLine("\nnum = " + num);
+            Console.WriteLine("Total items: " + queue.Count);
+
+            foreach (int i in queue)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ StackExample/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StackExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            //int num = stack[2];
+
+            foreach (int i in stack)
+                Console.WriteLine(i);
+
+
+            int num = stack.Pop();
+            Console.WriteLine("\n\nnum = " + num);
+            Console.WriteLine("Total items: " + stack.Count);
+
+            foreach (int i in stack)
+                Console.WriteLine(i);
+
+
+            num = stack.Peek();
+            Console.WriteLine("\n\nnum = " + num);
+            Console.WriteLine("Total items: " + stack.Count);
+
+            foreach (int i in stack)
+                Console.WriteLine(i);
+        }
+    }
+}
